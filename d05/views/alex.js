b4005c2c0765fac0303c4d63cccd846885480b46
DavideDaniel <davidedaniel@gmail.com>
2015-01-16 13:29:21 -0500

    pushed d3

---
+++ d05/views/alex.js
@@ -0,0 +1,9 @@
+exports.persons = {
+
+	alex: "Alex",
+	david: "David",
+
+	puts: function(){
+		console.log(this.alex);
+	}
+	};
---
+++ d05/views/test.js
@@ -0,0 +1,7 @@
+var alex = require('./alex');
+
+console.log(alex.persons.alex)
+console.log(alex.persons.david);
+
+alex.persons.puts();
+
