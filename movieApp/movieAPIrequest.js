1b6a3ecc9375fcf46b2fac03a7611b9cc37ffda2
DavideDaniel <davidedaniel@gmail.com>
2015-01-17 15:54:31 -0500

    going through movies one by one

--- movieApp/movieAPIrequest.js
+++ movieApp/movieAPIrequest.js
@@ -1,34 +1,47 @@
 var button = document.querySelector( "button" )
+
+var counter = 0;
+
+
+
 button.addEventListener( "click", function () {
     var movies = [ "Boyhood", "Whiplash", "Nightcrawler", "Gloria", "Gravity",
         "Her", "Skyfall", "The Imitation Game"
     ]
 
+    
     var input = document.querySelector( "input" )
     var img = document.querySelector( "img" )
     var description = document.querySelector( "description" )
     var xhr = new XMLHttpRequest(); //making an object from a constructor
 
     var movieRequest = function () {
-        movies.forEach( function ( movie ) {
-                var url = "http://omdbapi.com/?t=" + movie
+        
+            var url = "http://omdbapi.com/?t=" + movies[counter]
                 xhr.open( "GET", url );
+        // };
+
+
+        // movies.forEach( function ( movie ) {
+        //         var url = "http://omdbapi.com/?t=" + movie
+        //         xhr.open( "GET", url );
+
             }
 
-        )
-
-    }
+        // )
+    
 
 
     // var url = "http://omdbapi.com/?t=" + movies[ 0 ]
-    // movieRequest()
+    movieRequest()
     // console.log(movieRequest());
 
-    $.each(movies, function(i, val){ 
-        var url = "http://omdbapi.com/?t=" + val
-        xhr.open("GET", url)
-        debugger
-    })
+    // $.each(movies, function(i, val){ 
+    //     var url = "http://omdbapi.com/?t=" + val
+
+    //     xhr.open("GET", url)
+    // })
+    
     xhr.addEventListener( 'load', function ( e ) {
         var d = xhr.responseText //comes in in JSON so you will have to parse it
         var parsed = JSON.parse( d );
@@ -38,7 +51,8 @@ button.addEventListener( "click", function () {
         description.innerText = parsed.Plot;
 
     } )
-
     xhr.send();
+    
+    counter++
 
 } )
